55d2992ea14b376e44734906cb91fad847b30b08
Aashish <aashishkumar12376@gmail.com>
2023-01-26 10:13:01 +0530

    [ADDED] format.

--- helpers.py
+++ helpers.py
@@ -5,19 +5,23 @@ import numpy as np
 import requests
 
 
+def find_image_format(img):
+    return img.format
+
+
 def url_to_img(url):
   img = Image.open(BytesIO(requests.get(url).content))
-  img.save('images/sample1.jpg')
-  return None
+  format = find_image_format(img)
+  img.save(f'images/sample.{format}')
+  return format
 
 
 def find_white_background(imgpath, threshold=0.3):
-    url_to_img(imgpath)
-    imgArr = cv2.imread('images/sample1.jpg')
+    f = url_to_img(imgpath)
+    imgArr = cv2.imread(f'images/sample.{f}')
     background = np.array([255, 255, 255])
     percent = (imgArr == background).sum() / imgArr.size
     if percent >= threshold:
-        print(percent)
         return True
     else:
         return False
