1a9dfcdcc9aaf7a9f739f7d821b703d987078866
Amit Jain <8amitjain>
2023-06-27 10:47:38 +0530

    fixe

--- routes.py
+++ routes.py
@@ -21,11 +21,11 @@ def check_image():
                 {
                     'Image URL' : image, # Return image url
                     'White Background' : True if find_white_background(image) else False, # Return True if image has white background else False.
-                    'Human in Image' : True if is_human_in_image(image) else False, # Return True if human in image else False.
-                    'Greyscale Image' : True if is_greyscale(image) else False, # Return True if image is greyscale else False.
-                    'Blurry Image' : True if is_blurry(image) else False, # Return True if image is blurry else False.
-                    'Aspect Ratio 1' : True if is_aspect_ratio_1(image) else False, # Return True if image is aspect ratio is 1 else False.
-                    'Greater than 1000px' : True if is_image_size_greater_than_1000px(image) else False, # Return True if image is greater than else False.
+                    # 'Human in Image' : True if is_human_in_image(image) else False, # Return True if human in image else False.
+                    # 'Greyscale Image' : True if is_greyscale(image) else False, # Return True if image is greyscale else False.
+                    # 'Blurry Image' : True if is_blurry(image) else False, # Return True if image is blurry else False.
+                    # 'Aspect Ratio 1' : True if is_aspect_ratio_1(image) else False, # Return True if image is aspect ratio is 1 else False.
+                    # 'Greater than 1000px' : True if is_image_size_greater_than_1000px(image) else False, # Return True if image is greater than else False.
                 }
         )
     return jsonify(results)
